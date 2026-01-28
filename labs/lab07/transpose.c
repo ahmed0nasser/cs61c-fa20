ffeecfcf9e017ad9e868a5695971b6c28f5eb0bf
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2026-01-29 01:22:12 +0200

    Complete lab07/exercise3

--- labs/lab07/transpose.c
+++ labs/lab07/transpose.c
@@ -1,4 +1,5 @@
 #include "transpose.h"
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 /* The naive transpose function as a reference. */
 void transpose_naive(int n, int blocksize, int *dst, int *src) {
@@ -12,5 +13,13 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
 /* Implement cache blocking below. You should NOT assume that n is a
  * multiple of the block size. */
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
-    // YOUR CODE HERE
-}
+     for (int by = 0; by < n; by+=blocksize) {
+         for (int bx = 0; bx < n; bx+=blocksize) {
+             for (int y = by; y < MIN(by + blocksize, n); y++) {
+                 for (int x = bx; x < MIN(bx + blocksize, n); x++) {
+                     dst[y + x * n] = src[x + y * n];
+                 }
+             }
+         }
+     }
+ }
